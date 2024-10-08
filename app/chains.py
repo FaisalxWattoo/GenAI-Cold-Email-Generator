d503c174d9bfc33d5147bf045e91a3882903d1fc
FaisalxWattoo <faisalwattoo475@gmail.com>
2024-10-08 21:42:49 +0500

    First commit

---
+++ app/chains.py
@@ -0,0 +1,59 @@
+import os
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.exceptions import OutputParserException
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Chain:
+    def __init__(self):
+        self.llm = ChatGroq(temperature=0, groq_api_key=os.getenv("GROQ_API_KEY"), model_name="llama-3.1-70b-versatile")
+
+    def extract_jobs(self, cleaned_text):
+        prompt_extract = PromptTemplate.from_template(
+            """
+            ### SCRAPED TEXT FROM WEBSITE:
+            {page_data}
+            ### INSTRUCTION:
+            The scraped text is from the career's page of a website.
+            Your job is to extract the job postings and return them in JSON format containing the following keys: `role`, `experience`, `skills` and `description`.
+            Only return the valid JSON.
+            ### VALID JSON (NO PREAMBLE):
+            """
+        )
+        chain_extract = prompt_extract | self.llm
+        res = chain_extract.invoke(input={"page_data": cleaned_text})
+        try:
+            json_parser = JsonOutputParser()
+            res = json_parser.parse(res.content)
+        except OutputParserException:
+            raise OutputParserException("Context too big. Unable to parse jobs.")
+        return res if isinstance(res, list) else [res]
+
+    def write_mail(self, job, links):
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ## INSTRUCTION:
+            You are Faisal Riaz, a business development executive at Celystik Labs. Celystik Labs is a research and development studio specializing in Blockchain, AI,Data Engineering, ML and decentralized finance. Our core innovations include Intelligent Liquidity Provisioning Agents and TokenGPT, an AI-driven assistant for blockchain research and development. We are dedicated to optimizing liquidity strategies in DeFi platforms and assisting with token engineering using cutting-edge AI and Reinforcement Learning methodologies.
+
+            Your job is to write a cold email to the client regarding the job mentioned above, describing how Celystik Labs can fulfill their needs.
+
+            *🟡 Add the most relevant ones from the following links to showcase Celystik's solutions. 
+            Remember you are [Your Name], BDE at Celystik Labs. 
+            Do not provide a preamble.*
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+        chain_email = prompt_email | self.llm
+        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        return res.content
+
+if __name__ == "__main__":
+    print(os.getenv("GROQ_API_KEY"))
